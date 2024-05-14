8167e84c0941ae42a0066ac06d5924e38b4b0599
Tomas2p-Lenovo <91573621+Tomas2p@users.noreply.github.com>
2024-05-14 11:55:05 +0100

    Fix deploy v2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tomas2p Portfolio
 
+[![Deploy Astro site to Pages](https://github.com/Tomas2p/tomas2p.github.io/actions/workflows/astro.yml/badge.svg)](https://github.com/Tomas2p/tomas2p.github.io/actions/workflows/astro.yml)
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "static",
+  output: "hybrid",
   adapter: node({
     mode: "standalone",
   }),
